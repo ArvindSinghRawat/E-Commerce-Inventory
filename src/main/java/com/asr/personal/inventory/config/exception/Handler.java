a7436d5790f6a5b9edee093d7e5d775969b808df
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2022-11-06 13:07:01 +0530

    Added logs

--- src/main/java/com/asr/personal/inventory/config/exception/Handler.java
+++ src/main/java/com/asr/personal/inventory/config/exception/Handler.java
@@ -4,6 +4,7 @@ import com.asr.personal.inventory.dto.exception.ValidationErrorResponse;
 import com.asr.personal.inventory.dto.exception.Violation;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 class Handler {
 
@@ -25,6 +27,7 @@ class Handler {
       error.getViolations().add(
           new Violation(violation.getPropertyPath().toString(), violation.getMessage()));
     }
+    log.error("Error on request: ", e);
     return error;
   }
 
@@ -38,6 +41,7 @@ class Handler {
       error.getViolations().add(
           new Violation(fieldError.getField(), fieldError.getDefaultMessage()));
     }
+    log.error("Error on request: ", e);
     return error;
   }
 
